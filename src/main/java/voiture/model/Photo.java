3594f80bcfa4b36cdfbfcc062dd064139fe3dbca
Mickael-Rabearizoa <119034791+Mickael-Rabearizoa@users.noreply.github.com>
2024-02-09 18:08:07 +0100

    deploiement

--- src/main/java/voiture/model/Photo.java
+++ src/main/java/voiture/model/Photo.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "photo")
 @Table(name= "Photo")
 public class Photo {
 
--- src/main/java/voiture/repository/PhotoRepository.java
+++ src/main/java/voiture/repository/PhotoRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface PhotoRepository  extends JpaRepository<Photo,Integer> {
-     @Query(value = "select p from Photo as p where p.Id_Annonceutilisateur=?1")
+     @Query(value = "select p from photo as p where p.Id_Annonceutilisateur=?1")
      List<Photo> findPhotosById_Annonceutilisateur(int idAnnonceUtilisateur);
 }
